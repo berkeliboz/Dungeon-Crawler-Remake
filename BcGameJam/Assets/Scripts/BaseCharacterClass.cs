3db10878152158ba8dd4b139915c7883b4128177
berkeliboz <berkeboz96@gmail.com>
2019-01-19 21:51:57 -0800

    Added New Classes

---
+++ BcGameJam/Assets/Scripts/BaseCharacterClass.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCharacterClass : MonoBehaviour {
+
+
+    // Beginning of stats
+
+    public int baseHP;
+    public int baseMana;
+    public int baseArmor;
+    public int level;
+    public int totalHP;
+    public int bonusDamage;
+    public bool isAlive;
+    public int speed;
+    public int currentHP;
+    public int movementSpeed;
+
+    // End of stats
+
+
+    public void TakeDamage(int DamageTaken)
+    {
+
+    }
+}
--- BcGameJam/Assets/Scripts/Player.cs
+++ BcGameJam/Assets/Scripts/Player.cs
@@ -2,15 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : BaseCharacterClass {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Vector3 MovementVector;
+    float xAxisSpeed;
+    float yAxisSpeed;
+
+    public void Start()
+    {
+
+    }
+
+    public void Update()
+    {
+        xAxisSpeed = Input.GetAxis("Horizontal") * Time.deltaTime;
+        yAxisSpeed = Input.GetAxis("Vertical") * Time.deltaTime;
+
+        MovementVector = new Vector3(xAxisSpeed, yAxisSpeed,0f);
+        transform.position += MovementVector;
+
+    }
+
 }
